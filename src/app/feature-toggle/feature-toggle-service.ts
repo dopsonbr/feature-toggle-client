36d16bf7a5a5e6f302c6e8e39677e2f36d848cb7
dopsonbr <dopsonbr@gmail.com>
2018-04-16 12:23:56 -0400

    commit latest

--- src/app/feature-toggle/feature-toggle-service.ts
+++ src/app/feature-toggle/feature-toggle-service.ts
@@ -2,19 +2,26 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 
 import { HttpClient } from '@angular/common/http';
+import {RequestOptions} from '@angular/http';
 
 @Injectable()
 export class FeatureToggleService {
 
+  baseUrl = 'http://sell.apps.homedepot.com/router/UsomMessaging/configuration';
+
+
   constructor(private http: HttpClient) {}
 
   fetchStoreFeatureToggles(storeNumber: string, environment: string, productTeam: string): Observable<string[]> {
-    const url = `http://localhost:8080/featureToggles?storeNumber=${storeNumber}&environment=${environment}&productTeam=${productTeam}`;
-    return this.http.get<string[]>(url);
+    let headers = new Headers();
+    headers.set({'usom-correlationid': | 'username'});
+    headers.set('applictionname', 'admin');
+    const url = `${this.baseUrl}/storefeatureToggles?store=${storeNumber}&environment=${environment}&product=${productTeam}`;
+    return this.http.get<string[]>(url, { headers});
   }
 
   addStoreFeatureToggle(feature: string, storeNumber: string, environment: string, productTeam: string): Observable<string[]> {
-    const url = `http://localhost:8080/featureToggles?feature=${feature}&storeNumber=${storeNumber}&environment=${environment}&productTeam=${productTeam}`;
+    const url = `${this.baseUrl}/storefeatureToggles?feature=${feature}&storeNumber=${storeNumber}&environment=${environment}&productTeam=${productTeam}`;
     return this.http.put<string[]>(url, null);
   }
 
